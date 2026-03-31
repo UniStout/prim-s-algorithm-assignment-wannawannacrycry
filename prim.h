e0ab1c243ba10dffdaddb97cd3be7f0ac39952ba
bobacat-games <251046034+bobacat-ame@users.noreply.github.com>
2026-03-31 12:03:42 -0500

    header

--- prim.h
+++ prim.h
@@ -1,1 +1,18 @@
-//Please put header information in this file.
+#ifndef PRIM_H
+#define PRIM_H
+
+#include <vector>
+#include <string>
+using namespace std;
+
+
+int minKey(vector<int> &key, vector<bool> &mstSet);
+
+void printMST(vector<int> &parent, vector<vector<int>> &graph);
+
+
+void primMST(vector<vector<int>> &graph);
+
+vector<vector<int>> readGraphFromFile(const string &filename);
+
+#endif
